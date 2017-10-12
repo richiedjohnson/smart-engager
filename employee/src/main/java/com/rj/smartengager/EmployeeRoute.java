eb5de17f4204595beda556182b0587c3623f9e07
Daniel Johnson <johrichi@amazon.com>
2017-10-11 19:51:36 -0700

    Adding maven java run plugin

--- employee/src/main/java/com/rj/smartengager/EmployeeRoute.java
+++ employee/src/main/java/com/rj/smartengager/EmployeeRoute.java
@@ -1,9 +1,13 @@
 package com.rj.smartengager;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestParamType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.rj.smartengager.model.Employee;
 
 @Component
 public class EmployeeRoute extends RouteBuilder {
@@ -17,13 +21,27 @@ public class EmployeeRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		restConfiguration()
-			.component("netty-http")
+			.component("netty4-http")
 			.bindingMode(RestBindingMode.json)
 			.host(host)
-			.port(port);
+			.port(port)
+			.apiContextPath("/api-doc")
+			.apiProperty("api.title", "Employee API")
+			.apiProperty("api.version", "1.0")
+			.apiProperty("cors", "true");
+		
+		JacksonDataFormat formatEmp = new JacksonDataFormat(Employee.class);
 		
 		rest("/employee")
 			.get("/{id}")
+				.description("Find employee by id")
+				.param()
+					.name("id")
+					.type(RestParamType.path)
+					.description("Employee identificator")
+					.dataType("int")
+					.endParam()
+				.outType(Employee.class)
 			.to("bean:employeeService?method=findById(${header.id})");
 
 	}
